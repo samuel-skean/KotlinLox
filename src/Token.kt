87a28d62a0cb63c53b9dcb7c08acf2d3f6dfdd33
Samuel Skean <sskean2@uic.edu>
2024-12-24 20:16:04 -0600

    Ooh boy, a data class.
    Clearly the correct choice for this (and all the things like it). But I'm not really using it.

--- src/Token.kt
+++ src/Token.kt
@@ -1,3 +1,3 @@
-class Token(val type: TokenType, val lexeme: String, val literal: Any?, val lineNum: Int, val start: Int) {
+data class Token(val type: TokenType, val lexeme: String, val literal: Any?, val lineNum: Int, val start: Int) {
     override fun toString(): String = "Line Number: $lineNum Start position: $start $type $lexeme $literal"
 }
